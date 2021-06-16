2ad0c638750ece0ce6487d01e3a88b110487e067
Huy Tran <kingbazoka@gmail.com>
2021-06-16 13:49:46 -0700

    Add configurable port

--- main.js
+++ main.js
@@ -43,6 +43,6 @@ app.use('/graphql', graphqlHTTP({
   graphiql: true
 }));
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT);
 console.log(`Server is running at :${PORT}`);
